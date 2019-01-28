9e04eb6ff1a9298a59e7d98f4d839346249080bd
jruan <jiatong.ruan@gmail.com>
2019-01-27 22:41:07 -0500

    TwoSum

---
+++ TwoSum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        diff = {}
+        for i in range(len(nums)):
+            if nums[i] in diff:
+                return [diff[nums[i]], i]
+            else:
+                diff[target - nums[i]] = i
+        return []
